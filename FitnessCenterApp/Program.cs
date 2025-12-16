83d6d09760d04ba5fd88f05185fd66493b0d25a8
Raafatkalash <raftkalash93@gmail.com>
2025-12-16 23:46:58 +0300

    e

--- FitnessCenterApp/Program.cs
+++ FitnessCenterApp/Program.cs
@@ -3,16 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. خدمة MVC (Controllers + Views)
+
 builder.Services.AddControllersWithViews();
 
-// 2. تسجيل DbContext مع SqlServer (غيّر اسم ConnectionString إذا لازم)
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
-// 3. إعداد الـ Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -26,7 +24,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// 4. Route الافتراضي
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
